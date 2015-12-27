07242cea2eeb1bf7be5a4a8fa602728219d4d70c
j.lewis@dsl.pipex.com <j.lewis@dsl.pipex.com>
2015-12-27 20:24:54 +0000

    Liquibase checksum fix

--- src-pos/uk/chromis/pos/forms/JRootApp.java
+++ src-pos/uk/chromis/pos/forms/JRootApp.java
@@ -1095,6 +1095,26 @@ public class JRootApp extends JPanel implements AppView {
             Logger.getLogger(JRootApp.class.getName()).log(Level.SEVERE, null, ex);
         }
 
+        /*
+        int mb = 1024*1024;         
+        //Getting the runtime reference from system
+        Runtime runtime = Runtime.getRuntime();         
+        System.out.println("##### Heap utilization statistics [MB] #####");         
+        //Print used memory
+        System.out.println("Used Memory:"
+            + (runtime.totalMemory() - runtime.freeMemory()) / mb); 
+        //Print free memory
+        System.out.println("Free Memory:"
+            + runtime.freeMemory() / mb);         
+        //Print total available memory
+        System.out.println("Total Memory:" + runtime.totalMemory() / mb); 
+        //Print Maximum available memory
+        System.out.println("Max Memory:" + runtime.maxMemory() / mb);
+        */
+        
+        
+        
+        
         AboutDialog dialog = new AboutDialog();
         JPanel dialogPanel = new JPanel();
         MigLayout layout = new MigLayout("", "[fill]");
