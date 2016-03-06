7d108bca2c3c2a2e5c50446a057c7ad59db0348a
j.lewis@dsl.pipex.com <j.lewis@dsl.pipex.com>
2016-03-06 00:03:21 +0000

    fixes to migrate routine

--- src-pos/uk/chromis/pos/reports/PanelReportBean.java
+++ src-pos/uk/chromis/pos/reports/PanelReportBean.java
@@ -138,6 +138,18 @@ public class PanelReportBean extends JPanelReport {
      */
     public void setResourceBundle(String resourcebundle) {
         this.resourcebundle = resourcebundle;
+        
+                 /*    
+        String locale = AppConfig.getInstance().getProperty("user.language").equals("")
+                ? resourcebundle 
+                : resourcebundle + "_" + AppConfig.getInstance().getProperty("user.language");
+        
+        this.resourcebundle = AppConfig.getInstance().getProperty("user.country").equals("")
+                ? locale 
+                : locale + "_" + AppConfig.getInstance().getProperty("user.country");    
+       */
+        
+        
     }
     
     /**
